e24db6ef71efb21fcae847460f9395897b4b1703
Juan David Álvarez Sarmiento <75177549+JDavid550@users.noreply.github.com>
2021-03-15 18:05:08 -0500

    algoritmo para que solo un par quede estático

--- src/plugins/turnAroundcards.js
+++ src/plugins/turnAroundcards.js
@@ -1,21 +1,28 @@
-
-
+let clicklist = []
 const turnAroundCard = () =>{
     setTimeout(()=>{
-
         let cards = document.querySelectorAll('.card')
         let cardsFront = document.querySelectorAll('.card-front')
         console.log(cards);
+        
         for (let i = 0; i < cardsFront.length; i++) {
             cardsFront[i].addEventListener('click',()=>{
-                for (let j = 0; j < cards.length; j++) {
-                    
-                    cards[j].classList.remove('spin')
-                    
-                }
-                console.log(cards[i].children[1].children[0].attributes[1].nodeValue)
-                cards[i].classList.add('spin') 
-
+                clicklist.push(i)
+                console.log(clicklist)
+                if (cards[clicklist[clicklist.length-1]].children[1].children[0].attributes[1].nodeValue !== cards[clicklist[clicklist.length-2]].children[1].children[0].attributes[1].nodeValue) {
+                    for (let j = 0; j < cards.length; j++) {
+                            
+                        cards[j].classList.remove('spin')
+                    }
+                    cards[i].classList.add('spin')
+                } else {
+                    console.log('son iguales')
+                    cards[i].classList.add('spin')
+                } 
+                //console.log('Anterior es ' + clicklist[clicklist.length-2])
+                
+                //console.log(cards[i].children[1].children[0].attributes[1].nodeValue)
+                
             })
         }
     },2000)
