57d9bee126bc33e514d48942908e0eb83300133f
Juan David Álvarez Sarmiento <75177549+JDavid550@users.noreply.github.com>
2021-03-18 17:26:40 -0500

    autoplay con controlador añadido

--- src/plugins/autoplay.js
+++ src/plugins/autoplay.js
@@ -1,10 +1,24 @@
 function AutoPlay (){}
 
 AutoPlay.prototype.run = function(gameSession){
+    const buttonCountainer = document.getElementById('muteUnmuteContainer')
+    const muteButton = document.createElement('img')
+    muteButton.src = "https://img.icons8.com/windows/25/ffffff/high-volume--v3.png";
     const cardCollection = document.querySelector('.card-collection')
     cardCollection.addEventListener('click',()=>{
         gameSession.play();
+        buttonCountainer.appendChild(muteButton)
+
     })
+    muteButton.addEventListener('click', ()=>{
+        gameSession.muteUnmute();
+        if (muteButton.src == "https://img.icons8.com/windows/25/ffffff/high-volume--v3.png") {
+            muteButton.src = "https://img.icons8.com/windows/25/ffffff/no-audio--v3.png"
+        }else if(muteButton.src = "https://img.icons8.com/windows/25/ffffff/no-audio--v3.png"){
+            muteButton.src = "https://img.icons8.com/windows/25/ffffff/high-volume--v3.png"
+        }
+    })
+
 }
 
 export default AutoPlay;
