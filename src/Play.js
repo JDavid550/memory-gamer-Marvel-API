baa31f7e7acf82a75ea7e9098688e4afe0f36d74
Juan David Álvarez Sarmiento <75177549+JDavid550@users.noreply.github.com>
2021-03-21 21:54:41 -0500

    Cronometro terminado

--- src/Play.js
+++ src/Play.js
@@ -1,11 +1,13 @@
 import deploy from './deployData'
 import flipCard from './flipCard'
+import Chronometer from '../src/plugins/chronometer'
 
 
 ///This is the object the rest of the methods is created
 ///I'm using the prototype method to instance the object Play
 
 function Play(config) {
+    this.timerState = config.timerState
     this.media = config.audio
     this.plugins = config.plugins || []
     this._initPlugins();
@@ -38,6 +40,18 @@ Play.prototype.muteUnmute = function(){
         this.media.muted = false;
     }else{
         this.media.muted =true;
+    }
+}
+
+Play.prototype.toggleTimerStart = function(){
+    if (this.timerState == 0) {
+        this.timerState = 1
+        if (this.timerState == 1) {
+            Chronometer();
+        }
+        console.log('connected')
+    } else {
+        console.log('not connected')
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import AutoPlay from './plugins/autoplay'
 import Chronometer from './plugins/chronometer'
 
 const audio = document.querySelector('audio')
+const cardCollection = document.getElementById('card-collection')
 
 let gameSession = new Play({
+    timerState: 0,
     audio : audio,
-    plugins : [new Counter(), new AutoPlay(), new Chronometer()]
+    plugins : [
+        new Counter(), 
+        new AutoPlay(),
+    ]
 })
 
 gameSession.fetch();
 gameSession.flipCard();
 
+cardCollection.onclick = gameSession.toggleTimerStart()
 
 /////////////////////////
 
