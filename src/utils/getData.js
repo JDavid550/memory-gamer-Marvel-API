e14e7200ce8cb4b60760ae49f4695838357fab9e
Juan David Álvarez Sarmiento <75177549+JDavid550@users.noreply.github.com>
2021-03-29 21:52:49 -0500

    https protocol set in API link

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,5 +1,5 @@
-const API = 'http://gateway.marvel.com/v1/public/characters?limit=100&offset=200&ts=1&apikey=9d1213bcbc5b3fd8b46eb731651494fa&hash=0625ad92b2f50b8ee1ca09827ba36f69'
-const IMAGE_NOT_AVAILABLE = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+const API = 'https://gateway.marvel.com/v1/public/characters?limit=100&offset=200&ts=1&apikey=9d1213bcbc5b3fd8b46eb731651494fa&hash=0625ad92b2f50b8ee1ca09827ba36f69'
+const IMAGE_NOT_AVAILABLE = "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
 
 ///This is the first functionality of the project
 
